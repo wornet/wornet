8eaf7eb8dcf341afbbe616fb9e35f23ba63d62fb
KyleKatarn <kylekatarnls@gmail.com>
2014-09-02 20:46:06 +0200

    Redirect wget to stdout

--- core/system/disponibility.js
+++ core/system/disponibility.js
@@ -3,7 +3,7 @@ PORT = (process.env.PORT || 8002);
 SERVICE = (process.env.SERVICE || 'wornetint');
 exec = require("child_process").exec;
 
-exec('wget http://localhost:' + PORT + '/git-status', function (err, data, errm) {
+exec('wget -qO- http://localhost:' + PORT + '/git-status', function (err, data, errm) {
 	console.log([err, data, errm]);
 	if(data !== 'OK') {
 		var cmd = 'service ' + SERVICE + ' update';
