2b13ff83081a316958e327283c60e2585bfc8f1d
KyleKatarn <kylekatarnls@gmail.com>
2014-09-02 20:43:34 +0200

    Use port to check availaibility

--- core/system/disponibility.js
+++ core/system/disponibility.js
@@ -1,9 +1,9 @@
-var HOST, SERVICE, exec;
-HOST = (process.env.HOST || 'int.wornet.com');
+var PORT, SERVICE, exec;
+PORT = (process.env.PORT || 8002);
 SERVICE = (process.env.SERVICE || 'wornetint');
 exec = require("child_process").exec;
 
-exec('wget http://' + HOST + '/git-status', function (err, data, errm) {
+exec('wget http://localhost:' + PORT + '/git-status', function (err, data, errm) {
 	console.log([err, data, errm]);
 	if(data !== 'OK') {
 		var cmd = 'service ' + SERVICE + ' update';
