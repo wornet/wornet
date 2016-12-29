50ec475d5c82b856e5dc3634141f380f182fd559
KyleKatarn <kylekatarnls@gmail.com>
2016-12-29 17:46:46 +0100

    Remove nodemon

--- install.js
+++ install.js
@@ -4,7 +4,6 @@ require(__dirname + "/core/system/command.js")([
 
     "npm install kraken-js",
     "npm install -g coffee-script",
-    "npm install -g nodemon",
     "npm install -g mocha",
     "npm install -g codo"
 
